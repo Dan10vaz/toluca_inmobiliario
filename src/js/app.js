d9d67280a10d4026493ef604573fb9eb712e189a
DanielGesto <danielcristophervazquezayala@gmail.com>
2021-12-20 16:16:30 -0600

    6

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
     darkMode();
 
+    slider();
+
     serviceWorker();
+
 });
+
+function slider(){
+    const slider = document.querySelector('#slider');
+    let sliderSection = document.querySelectorAll('.slider__section');
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+    const btnLeft = document.querySelector('#btn-left');
+    const btnRight = document.querySelector('#btn-right');
+
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+    function moverDerecha() {
+        let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "all 0.5s";
+
+        setTimeout(function(){
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+            slider.style.marginLeft = "-100%";
+        }, 500);
+    }
+
+    function moverIzquierda() {
+        let sliderSection = document.querySelectorAll('.slider__section');
+        let sliderSectionLast = sliderSection[sliderSection.length -1];
+        slider.style.marginLeft = "0";
+        slider.style.transition = "all 0.5s";
+
+        setTimeout(function(){
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+            slider.style.marginLeft = "-100%";
+        }, 500);
+    }
+
+    btnRight.addEventListener('click', moverDerecha);
+    btnLeft.addEventListener('click', moverIzquierda);
+
+    setInterval(function(){
+        moverDerecha();
+    }, 4000);
+}
 
 function serviceWorker() {
     if('serviceWorker' in navigator)
@@ -15,9 +61,7 @@ function serviceWorker() {
     else {
     console.log('Service Workers no soportados')
     }
-
 }
-
 
 function darkMode(){
 
