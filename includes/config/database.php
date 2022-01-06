c4f796dba83c6e116ea539e2e2e89ee70554e766
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-01-06 15:20:47 -0600

    12

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,7 +4,7 @@
         $db = mysqli_connect('localhost', 'u745111728_root', 'Root1234', 'u745111728_bienes_raices');
 
         if(!$db) {
-            echo "Error no se pudo conectar";
+            echo "Error no se pudo conectar a la base de datos";
             exit;
         }
 
